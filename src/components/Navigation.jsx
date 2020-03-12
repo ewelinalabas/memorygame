39d181253f347e1e6a71c2f3ee3bda5eb7899ac8
Ewelina Łabas <ewelina.labas@gmail.com>
2020-03-12 16:43:02 +0100

    Add react-bootstrap; change Navigation styling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,40 +1,19 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import { act } from 'react-dom/test-utils';
+import React from 'react';
+import { Navbar, Nav } from "react-bootstrap";
 
 export const Navigation = ({ location }) => {
-  // const [activePage, setActivePage] = useState("Game")
-  const handleClick = (value) => {
-
-  }
-
-  // const navLinks = [
-  // <NavLink to="/" key="1" value="Game" className="nav-item nav-link" onClick={(event => handleClick(event.target))}>Game</NavLink>,
-  // <NavLink to="/scoreboard" value="Scoreboard" key="2" className="nav-item nav-link">Scoreboard</NavLink>]
-
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="navbar-collapse" id="navbarNavAltMarkup">
-        <span className="navbar-brand mb-0 h1">Memory</span>
-        <div activeKey={location.pathname} className="navbar-nav">
-          <NavLink 
-            // activeClassName={activePage == "Game" ? "active" : ""} 
-            to="/" className="nav-item nav-link" 
-            // onClick={() => setActivePage("Game")}
-          >
+    <Navbar bg="light" variant="light">
+        <Navbar.Brand >Memory</Navbar.Brand>
+        <Nav activeKey={location.pathname} className="mr-auto">
+          <Nav.Link href="/" className="nav-item nav-link">
             Game
-          </NavLink>
+          </Nav.Link>
 
-          <NavLink
-            // activeClassName={activePage == "Scoreboard" ? "active" : ""} 
-            to="/scoreboard" className="nav-item nav-link"
-            // onClick={() => setActivePage("Scoreboard")}
-            >
-              Scoreboard
-          </NavLink>
-          {/* {navLinks} */}
-        </div>
-      </div>
-    </nav>
+          <Nav.Link href="/scoreboard" className="nav-item nav-link">
+            Scoreboard
+          </Nav.Link>
+        </Nav>
+    </Navbar>
   )
 }
