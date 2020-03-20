38f71de8e8dad897660caba62d1e8421aa5abcd0
Ewelina Łabas <ewelina.labas@gmail.com>
2020-03-20 19:50:15 +0100

    Add docs as production app

---
+++ docs/precache-manifest.d64d0484799749329c1c93fa08446d20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d1e6d789b5749f340bcc5697f48f90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6756e06dd102d090b88",
+    "url": "/static/css/main.57bcd97c.chunk.css"
+  },
+  {
+    "revision": "15e0c85cca360b690723",
+    "url": "/static/js/2.23cd34ac.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.23cd34ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6756e06dd102d090b88",
+    "url": "/static/js/main.527eb85f.chunk.js"
+  },
+  {
+    "revision": "965bd80d014101e00a7c",
+    "url": "/static/js/runtime-main.0f9fcdb6.js"
+  }
+]);
